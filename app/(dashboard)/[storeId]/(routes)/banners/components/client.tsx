4317cf0aaba408243b4f7f27c93ea6920d6c357d
daffaabp <daffa.budi2003@gmail.com>
2024-11-30 13:28:18 +0700

    add dataTable admin

--- app/(dashboard)/[storeId]/(routes)/banners/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/banners/components/client.tsx
@@ -1,20 +1,24 @@
 'use client'
 
-import { Banner } from "@prisma/client"
 import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
 import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
+import { BannerColumn, columns } from "./columns"
+import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
 
 interface BannerClientProps {
-    data: Banner[]
+    data: BannerColumn[]
 }
 export const BannerClient: React.FC<BannerClientProps> = ({
     data
 }) => {
+
     const router = useRouter();
     const params = useParams();
+
     return (
         <>
             <div className="flex items-center justify-between">
@@ -25,6 +29,10 @@ export const BannerClient: React.FC<BannerClientProps> = ({
                 </Button>
             </div>
             <Separator />
+            <DataTable data={data} columns={columns} searchKey="label" />
+            <Heading title="API" description="API untuk Banners" />
+            <Separator />
+            <ApiList namaIndikator="banners" idIndikator="bannerId" />
         </>
 
     )
--- app/(dashboard)/[storeId]/(routes)/banners/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/banners/page.tsx
@@ -1,5 +1,8 @@
 import db from "@/lib/db";
 import { BannerClient } from "./components/client";
+import { BannerColumn } from "./components/columns";
+
+import { format } from "date-fns"
 
 const BannersPage = async ({
     params
@@ -14,10 +17,17 @@ const BannersPage = async ({
             createdAt: "desc"
         }
     })
+
+    const formattedBanners: BannerColumn[] = banners.map((item) => ({
+        id: item.id,
+        label: item.label,
+        createdAt: format(item.createdAt, "MMM do, yyyy"),
+    }))
+
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BannerClient data={banners} />
+                <BannerClient data={formattedBanners} />
             </div>
         </div>
     );
