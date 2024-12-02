52d96d05e2be04d9b08bd440ef4f6d44671b3540
daffaabp <daffa.budi2003@gmail.com>
2024-12-02 07:31:03 +0700

    Update columns.tsx

--- app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -7,7 +7,7 @@ import { CellAction } from "./cell-action"
 // You can use a Zod schema here if you want.
 export type ProductColumn = {
   id: string
-  label: string
+  name: string
   price: string
   category: string
   isFeatured: boolean
